2d4d072d839f4726fe7b42b19d41c8d53349b130
BrentHickman <brenthickman@ymail.com>
2023-06-01 14:57:49 -0700

    added test image sc with some opacity effects. Cool

--- components/QuoteGenerator/QuoteGeneratorElements.tsx
+++ components/QuoteGenerator/QuoteGeneratorElements.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export const GradientBackgroundCon = styled.div`
   background: linear-gradient(to right, #000000, #ffffff);
   background-size: 400% 400%;
-  animation: gradient 6s ease infinite;
+  animation: gradient 15s ease infinite;
   height: 100vh;
   width: 100vw;
   @keyframes gradient {
@@ -19,4 +19,32 @@ export const GradientBackgroundCon = styled.div`
       background-position: 0% 50%;
     }
   }
+`;
+
+export const BackgroundImage1 = styled(Image)`
+  position: relative;
+  z-index: 1;
+  margin-left: 120px;
+  margin-top: 10px;
+  opacity: .8;
+`;
+
+export const BackgroundImage2 = styled(Image)`
+  position: fixed;
+  z-index: 1;
+  margin-left: -120px;
+  margin-top: 10px;
+  animation: opacity 8s ease infinite;
+  @keyframes opacity {
+    0%{
+      opacity: .9;
+    }
+    50% {
+      opacity: .3;
+    }
+    100% {
+      opacity: .9;
+    }
+  }
+  
 `;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
 
+// Components
+import { BackgroundImage1,BackgroundImage2,GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+
+// Assets
+import BurntAir2 from '../assets/BurntAir_2.jpg'
+import BakedChrome from '../assets/BakedChrome.jpg'
 
 export default function Home() {
   return (
@@ -15,6 +20,16 @@ export default function Home() {
       </Head>
       {/* Background */}
       <GradientBackgroundCon>
+        <BackgroundImage1
+          src={BurntAir2}
+          height="300"
+          alt="smokeyairbackground"
+        />
+        <BackgroundImage2
+          src={BakedChrome}
+          height="300"
+          alt="orangechromebackground"
+        />
         
       </GradientBackgroundCon>
     </>
